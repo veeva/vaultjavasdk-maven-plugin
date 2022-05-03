c2f6f63060924d2771ecf11c8723974ecfc49260
Isak Thomas <55297873+ithomas-veeva@users.noreply.github.com>
2022-05-03 09:31:43 -0400

    v22.1.0

--- src/main/java/com/veeva/vault/sdk/vaultjavasdk/ValidatePlugin.java
+++ src/main/java/com/veeva/vault/sdk/vaultjavasdk/ValidatePlugin.java
@@ -1,17 +1,9 @@
 package com.veeva.vault.sdk.vaultjavasdk;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-
+import com.veeva.vault.sdk.vaultjavasdk.utilities.ErrorHandler;
 import com.veeva.vault.sdk.vaultjavasdk.utilities.PackageManager;
-import com.veeva.vault.sdk.vaultjavasdk.utilities.VaultAPIService;
+import com.veeva.vault.vapil.api.model.response.ValidatePackageResponse;
+import org.apache.maven.plugins.annotations.Mojo;
 
 /**
  * Goal that validates the last modified VPK in the "deployment/packages" directory against the validation API endpoint.
@@ -19,69 +11,40 @@ import com.veeva.vault.sdk.vaultjavasdk.utilities.VaultAPIService;
  */
 
 @Mojo( name = "validate", requiresProject = false)
-public class ValidatePlugin extends AbstractMojo {
-
-	protected static boolean authStatus;
-	
-	@Parameter( property = "apiVersion", defaultValue = "v18.3" )
-	protected  String apiVersion = "";
-	@Parameter( property = "vaulturl", defaultValue = "" )
-	protected String vaultUrl = "";
-	@Parameter( property = "username", defaultValue = "" )
-	protected String username = "";
-	@Parameter( property = "password", defaultValue = "" )
-	protected String password = "";
-	@Parameter( property = "sessionId", defaultValue = "" )
-	protected String sessionId = "";
-	@Parameter( property = "package", defaultValue = "" )
-	protected String packageName = "";
-	@Parameter( property = "packageId", defaultValue = "" )
-	protected String packageId = "";
-	@Parameter( property = "source" )
-	protected Source source = new Source();
+public class ValidatePlugin extends BaseMojo {
 	
 	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		apiVersion = "/api/" + apiVersion;
-		VaultAPIService vaultClient = new VaultAPIService(apiVersion, vaultUrl, username, password, sessionId);
-		
+	public void execute() {
+		//Initializes an Authentication API connection.
+		initializeVaultClient();
 		try {
-			//Initializes an Authentication API connection.
-			
-			authStatus = vaultClient.verifySession();
-			
-			if (authStatus == true) {
+			if (vaultClient.validateSession()) {
 				//Validates the defined VPK against the specified vault.
-				System.out.println("");
-				String status = null;
-				
+				System.out.println("Session is valid");
 				if (!packageName.equals("")) {
 					PackageManager.setPackagePath(packageName);
 				}
 				
 				if (PackageManager.getPackagePath() != null) {
-					status = vaultClient.validatePackage(PackageManager.getPackagePath());
+					ValidatePackageResponse response = PackageManager.validatePackage(vaultClient, PackageManager.getPackagePath());
+
+					if (response.isSuccessful()) {
+						System.out.println("Validation successful");
+					} else {
+						ErrorHandler.logErrors(response);
+					}
+
 				}
 				else {
 			        System.out.println("Cannot validate package. There is no VPK in '<PROJECT_DIRECTORY>/deployment/packages/'.");
 				}			
+			} else {
+				System.out.println("Not a valid session. Check the login details in the pom file.");
 			}
-		} catch (MalformedURLException e) {
-			getLog().info(e.toString());
-		} catch (ProtocolException e) {
-			getLog().info(e.toString());
-		} catch (IOException e) {
-			getLog().info(e.toString());
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
